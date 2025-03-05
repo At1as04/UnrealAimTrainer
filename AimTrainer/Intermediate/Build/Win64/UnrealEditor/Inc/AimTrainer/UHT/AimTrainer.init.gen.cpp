c2a2c0b1e76f843b66f4ec09a46cd6ab57727863
Atlas1004 <EthanSchneiderGames@gmail.com>
2025-03-04 20:28:54 -0600

    Initial Commit

---
+++ AimTrainer/Intermediate/Build/Win64/UnrealEditor/Inc/AimTrainer/UHT/AimTrainer.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeAimTrainer_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_AimTrainer;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_AimTrainer()
+	{
+		if (!Z_Registration_Info_UPackage__Script_AimTrainer.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/AimTrainer",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0x2C7D250C,
+				0xBC257514,
+				METADATA_PARAMS(0, nullptr)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_AimTrainer.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_AimTrainer.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_AimTrainer(Z_Construct_UPackage__Script_AimTrainer, TEXT("/Script/AimTrainer"), Z_Registration_Info_UPackage__Script_AimTrainer, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x2C7D250C, 0xBC257514));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
